ebe761417c3b129d1fcf1bc747facf123b3a7749
Davi <davicamiloribeiro@gmail.com>
2021-10-23 23:42:10 -0300

    Base inicial do projeto

---
+++ database/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize(
+    
+    'controlefinancas','root','12345',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        timezone: "-03:00"
+    }
+    
+);
+
+module.exports = connection;
